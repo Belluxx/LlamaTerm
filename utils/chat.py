5331320a7a32f11f7c07edf82e2c8376c19f5dd8
Belluxx <78388441+Belluxx@users.noreply.github.com>
2023-11-16 00:31:52 +0100

    Better generator context awareness

--- utils/chat.py
+++ utils/chat.py
@@ -51,11 +51,19 @@ class Chat:
     def generate_reply(self) -> str:
         full_reply = ""
         n_current_tokens = 0
+        free_context = self.context_available()
+
+        if free_context <= 0:
+            self.context_exceeded()
         
         start_time = time()
-        for token in self.model.generate(tokens=self.tokens, reset=False):
-            if token == self.model.token_eos(): break
-            if n_current_tokens >= self.n_generate: break
+        for token in self.model.generate(tokens=self.tokens):
+            if token == self.model.token_eos():
+                break
+            if n_current_tokens >= self.n_generate:
+                break
+            if free_context - n_current_tokens <= 0: 
+                self.context_exceeded()
             
             self.tokens.append(token)
             n_current_tokens += 1
@@ -74,6 +82,7 @@ class Chat:
 
         return full_reply
 
+
     def check_model_impersonation(self, full_reply: str, actor: str) -> tuple[bool, str]:
         interrupt = False
         if self.prefixes[actor] in full_reply:
@@ -81,12 +90,26 @@ class Chat:
             interrupt = True
         return interrupt, full_reply
 
+
     def tokenize_text(self, text: str) -> list[int]:
         return self.model.tokenize(bytes(text, 'UTF-8'), add_bos=False)
 
 
+    def tokens_used(self) -> int:
+        return len(self.tokens)
+
+
+    def context_available(self) -> int:
+        return self.model.n_ctx() - self.tokens_used()
+    
+
+    def context_exceeded(self):
+        print('[ERROR] Context exceeded.')
+        exit(0)
+
+
     def print_stats(self):
         if self.generation_time == 0: return
-        print(f'Tokens used: {len(self.tokens)}')
-        print(f'Tokens left: {self.model.n_ctx() - len(self.tokens)}')
+        print(f'Tokens used: {self.tokens_used()}')
+        print(f'Tokens left: {self.context_available()}')
         print(f'Tokens generated per second: {self.n_tokens_generated / self.generation_time:.2f}')
